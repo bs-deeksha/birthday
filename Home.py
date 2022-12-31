043d41bdc467f32d9c0d621cc91e018d3db13ba2
bs-deeksha <117064540+bs-deeksha@users.noreply.github.com>
2022-12-31 15:27:06 -0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import time
+st.set_page_config(layout="centered")
+st.title("Hello Ji!")
+time.sleep(2)
+st.title("En samachara?")
+time.sleep(2)
+st.title("Uta Ayta???")
+time.sleep(2)
+st.title("⬅️ Sari choose a page")
+time.sleep(2)
